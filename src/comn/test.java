22fed446276c53973c26e5cbe2d2db09a04c4f45
yan <3140585910@qq.com>
2025-07-15 15:52:09 +0800

    JAVA基础学习

--- src/comn/test.java
+++ src/comn/test.java
@@ -9,9 +9,9 @@ public class test {
     }
 
     public  static void testForEachOrdered() {
-        Stream.of(1,5,-9,0,-5,2,5,8).parallel().forEach(System.out::print);
+        Stream.of(1,5,-9,0,-5,2,5,8).parallel().forEach(System.out::println);
         System.out.println("===================================================");
         // 在并行遍历时，forEachOrdered将顺序遍历元素
-        Stream.of(1,5,-9,0,-5,2,5,8).parallel().forEachOrdered(System.out::print);
+        Stream.of(1,5,-9,0,-5,2,5,8).parallel().forEachOrdered(System.out::println);
     }
 }
